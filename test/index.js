277c4d0a4ec8ca06a0cc94f10ea41a445e5dc665
hpf1908 <hpf1908@gmail.com>
2013-04-23 00:32:53 +0800

    fix bugs , compatibale with firefox

--- test/index.js
+++ test/index.js
@@ -31,8 +31,8 @@ define(function(require, exports, module) {
     return {
         init : function() {
 
-            // var testFunc = this['test root childs'];
-            var testFunc = this['test has with childs'];
+            var testFunc = this['test root childs'];
+            // var testFunc = this['test has with childs'];
             // var testFunc = this['test has edit'];
             // var testFunc = this['test export json'];
             // var testFunc = this['test import from json'];
